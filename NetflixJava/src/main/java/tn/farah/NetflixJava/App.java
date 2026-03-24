3bd6748743ca32d6cad1350de41552d00b9a822f
farah <farah@PC_of_Farah>
2026-03-24 21:10:28 +0100

    FIRSTPAGE

--- NetflixJava/src/main/java/tn/farah/NetflixJava/App.java
+++ NetflixJava/src/main/java/tn/farah/NetflixJava/App.java
@@ -20,7 +20,7 @@ public class App extends Application {
 	@Override
     public void start(Stage stage) throws IOException {
 		
-		 scene = new Scene(loadFXML("firstPage"), 640, 480);
+		 scene = new Scene(loadFXML("MainView"), 640, 480);
 	        stage.setScene(scene);
 	        stage.show();
 	    }
